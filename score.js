636f2951956066b2f878f18bf6f92451a9e0c218
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-28 18:13:25 +0000

    Update score.js

--- score.js
+++ score.js
@@ -8,7 +8,7 @@ var canvasModule = angular.module('app', []).
 		var context = canvas.getContext('2d');
 			
 		$scope.margin = 50;
-		$scope.lineHeight = 40;
+		$scope.lineHeight = 48;
 		
 		$scope.lines = [
 		   
