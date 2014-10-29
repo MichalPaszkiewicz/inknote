5f31b6515d13036dbd26a17f275a5c64d6e4ba65
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-29 22:43:18 +0000

    Update score.js

--- score.js
+++ score.js
@@ -212,7 +212,7 @@ var canvasModule = angular.module('app', []).
 		}
 		
 		$scope.startup = function(){
-				if(time == 200){
+				if(time == 100){
 					context.fillStyle = "white";
 					context.font = "bold 16px Arial";
 					context.fillText("inknote", canvas.width / 2, canvas.height / 2);
