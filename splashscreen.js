ad67d1bc0d02b060ca1d87d8e7ad73f631d4f63d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-29 22:28:58 +0000

    Update splashscreen.js

--- splashscreen.js
+++ splashscreen.js
@@ -7,8 +7,8 @@ function animate(particles, delta, last)
         var p = particles[i];
         p.x += Math.cos(p.angle) * 4 + Math.random() * 2 - Math.random() * 2;
         p.y += Math.sin(p.angle) * 4 + Math.random() * 2 - Math.random() * 2;
-        p.life -= delta / 10;
-        p.size -= delta / 20;
+        p.life -= delta / 100;
+        p.size -= delta / 200;
         
         if (p.size <= 0)
         {
