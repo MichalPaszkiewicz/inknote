86b4cb3187121cd07a23ddd9a724aa508a28167c
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-29 21:27:49 +0000

    Update splashscreen.js

--- splashscreen.js
+++ splashscreen.js
@@ -2,16 +2,24 @@ var splashscreen = function(canvas, context, time){
 	
 	var tempWidth = 0;
 	
-	if( time < 180){
-		tempWidth = 200 - time;
+	if( time < 300){
+		if(time < 180){
+			tempWidth = 100 - time / 2;
+		}
+		else if(time < 200){
+			tempWidth = 10 + (time - 180) * 5;
+		}
+		else{
+			tempWidth = 110;
+		}
+		
+		context.clearRect(0,0,canvas.width,canvas.height);
+		context.beginPath();
+		context.fillStyle = "red";
+		context.arc(canvas.width / 2, canvas.height / 2, tempWidth, 0, 2 * Math.PI, false);
+		context.fill();
 	}
 	else{
-		tempWidth = 2 * time - 360;
+		
 	}
-	
-	context.clearRect(0,0,canvas.width,canvas.height);
-	context.beginPath();
-	context.fillStyle = "red";
-	context.arc(canvas.width / 2, canvas.height / 2, tempWidth, 0, 2 * Math.PI, false);
-	context.fill();
 }
