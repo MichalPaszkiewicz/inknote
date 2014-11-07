4629d1459e3e945e1cead7fca9da68cbbbc6b6fa
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-07 18:11:31 +0000

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -7,3 +7,14 @@ Array.prototype.allItemsAre = function(xAndY){
 	}
 	return true;
 }
+
+Array.prototype.countWhere = function(xAndY){
+	var count = 0;
+	for(var i = 0; i < this.length; i++)
+	{
+		if(xAndY(this[i])){
+			count++;
+		}
+	}
+	return count;
+}
