a43b062daa958e406828390203a9717149aa6d57
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-08 09:10:23 +0000

    Update score.js

--- score.js
+++ score.js
@@ -264,6 +264,7 @@ var canvasModule = angular.module('app', ['monospaced.mousewheel']).
 					var lineIndex = Math.ceil(barIndex / 5);
 					for(var instrumentIndex = 0; instrumentIndex < $scope.instruments.length; instrumentIndex++){
 						if($scope.lines[lineIndex].instruments[instrumentIndex] == null || $scope.lines[lineIndex].instruments[instrumentIndex] == undefined){
+							var instrumentName = $scope.instruments[instrumentIndex].name;
 							var newInstrument = {name: instrumentName, id: instrumentIndex, bars: [], visible: true};
 						}
 						$scope.lines[lineIndex].instruments[instrumentIndex].bars.push($scope.instruments[instrumentIndex].bars[barIndex]);
