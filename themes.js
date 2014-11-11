40c7f736eaeecff196d35e0831e80b3390bc4e42
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-11 13:01:31 +0000

    Create themes.js

---
+++ themes.js
@@ -0,0 +1,11 @@
+angular.module('theme', []).
+	controller("themeControl", function canvasCtrl($scope){
+		$scope.themes = [
+			{name: "standard", url: "styles.css"},
+			{name: "dark", url: "themes/dark/styles.css"},
+			{name: "light", url: "themes/light/styles.css"},
+			{name: "sepia", url: "themes/sepia/styles.css"}
+		];
+		
+		$scope.currentThemeIndex = 0;
+	});
