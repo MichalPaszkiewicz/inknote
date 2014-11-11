874e725654e2f6462df028768c0b66f86aa13b7e
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-11 13:09:01 +0000

    Update themes.js

--- themes.js
+++ themes.js
@@ -8,4 +8,12 @@ angular.module('themes', []).
 		];
 		
 		$scope.currentThemeIndex = 0;
+		
+		$scope.setTheme(theme){
+			for(var i = 0; i < $scope.themes.length; i++){
+				if(theme == $scope.themes[i]){
+					$scope.currentThemeIndex = i;
+				}
+			}
+		}
 	});
