ab6c64080b5413c50a2e1e0539148031dc828beb
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-13 13:56:09 +0000

    Update score.js

--- score.js
+++ score.js
@@ -200,11 +200,11 @@ var canvasModule = angular.module('app', ['monospaced.mousewheel', 'keypress']).
 			var itemY = ($scope.lineHeight/8) * Math.round((y - 200) / ($scope.lineHeight/8));
 			
 			//relative y
-			var relY = y; // - $scope.windowScroll;
+			var relY = y - $scope.windowScroll;
 			
 			for(var i = 0; i < $scope.lines.length; i++){
-				if(   ($scope.lines[i].y - $scope.lineHeight/2)  < relY 
-				&&    ($scope.lines[i].y + $scope.getFullLineHeight($scope.lines[i]) + $scope.lineHeight/2)    >  relY){
+				if(   ($scope.lines[i].y - $scope.lineHeight/2)  < y 
+				&&    ($scope.lines[i].y + $scope.getFullLineHeight($scope.lines[i]) + $scope.lineHeight/2)    >  y){
 					for(var j = 0; j <  $scope.lines[i].instruments.length; j++){
 						if(	($scope.lines[i].instruments[j].y)   <   relY 
 						&&   	($scope.lines[i].instruments[j].y + $scope.lineHeight)    > relY){
