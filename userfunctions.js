563f53746f78325b7b8a5534300d20fbf5c37884
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-17 09:31:47 +0000

    Create userfunctions.js

---
+++ userfunctions.js
@@ -0,0 +1,6 @@
+/*todo: in future, this file will hold many arrays of user defined functions. The user will be able to add functions to the arrays to, for example, save data to their own database on save, etc. */
+
+var userFunctions = {};
+
+userFunctions.onSave = [];
+userFunctions.onLoad = [];
