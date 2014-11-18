a0d93d7b2d6be4521cd71a024417a86c14444b16
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-18 09:34:36 +0000

    Update script.js

--- developer/script.js
+++ developer/script.js
@@ -4,6 +4,9 @@
 var forumModule = angular.module('app', []).
 	controller("forumCtrl", function forumCtrl($scope, $window){
 		
-		
+		$scope.forum = {
+			threads: [{subject: "blah blah blah", posts: [{user: "user1", message: "hi how u doing"}, {user: "user2", message: "lol wassup"}]},
+				{subject: "how to add your own functions", posts: [{user: "user1", message: "hi how u doing"}, {user: "user2", message: "lol wassup"}]}]
+		};
 		
 	});
