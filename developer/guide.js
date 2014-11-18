a00fb32097989922327b91ecf717290911cd56b5
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-18 14:08:05 +0000

    Update guide.js

--- developer/guide.js
+++ developer/guide.js
@@ -3,12 +3,15 @@ developerKingdomModule.
 		
 		$scope.sections = [{name: "Introduction", html: "blah blah blah <h1>Lololol</h1>"},{name: "Adding plugins", html: "<h3>Na na na na na nana heeeeeey jude</h3><h3>Hey judey judey jude</h3>"}];
 		
-		$scope.setSection = function(thread){
-			$scope.currentThread = thread;
-			$scope.currentThreadOn = true;
-		}
-		
 		$scope.currentSection = {};
 		$scope.currentSectionOn = false;
 		
+		$scope.setSection = function(section){
+			$scope.currentSection = section;
+			$scope.currentSectionOn = true;
+		}
+		
+		$scope.backToSections = function(){
+			$scope.currentSectionOn = false;
+		}
 	})
