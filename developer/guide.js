d2fb12a345c3195554876d471f5501a8544da6e4
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-18 17:23:02 +0000

    Update guide.js

--- developer/guide.js
+++ developer/guide.js
@@ -1,7 +1,9 @@
 developerKingdomModule.
 	controller("guideCtrl", function guideCtrl($scope, $window, $sce){
 		
-		$scope.sections = [{name: "Introduction", htmlText: "blah blah blah <h1>Lololol</h1>"},{name: "Adding plugins", htmlText: "<h3>Na na na na na nana heeeeeey jude</h3><h3>Hey judey judey jude</h3>"}];
+		$scope.sections = [{name: "Introduction", htmlText: "<p>This site is a guide to all developers wishing to contribute to or integrate with the inknote project.</p>"
+								+ "<p>The aim of this project is to create a free, open source, browser based scoring program that will be both user and developer friendly.</p>"
+		},{name: "Adding plugins", htmlText: "<h3>Na na na na na nana heeeeeey jude</h3><h3>Hey judey judey jude</h3>"}];
 		
 		$scope.currentSection = {};
 		$scope.currentSectionOn = false;
