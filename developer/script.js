3eabc966e5566baf930ed1aaac61e470089d98f1
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-18 12:48:33 +0000

    Update script.js

--- developer/script.js
+++ developer/script.js
@@ -6,7 +6,7 @@ var forumModule = angular.module('app', []).
 		
 		$scope.forum = {
 			threads: [{subject: "blah blah blah", posts: [{user: "user1", message: "hi how u doing"}, {user: "user2", message: "lol wassup"}]},
-				{subject: "how to add your own functions", posts: [{user: "user1", message: "hi how u doing"}, {user: "user2", message: "lol wassup"}]}]
+				{subject: "how to add your own functions", posts: [{user: "user1", message: "asdfasdf"}, {user: "user2", message: "j jajajajajajh"}]}]
 		};
 		
 		$scope.currentThread = {};
