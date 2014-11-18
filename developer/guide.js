aaeee4b0bebdc29d53eb3187ca2d6aaddbd4f2bf
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-18 14:28:44 +0000

    Update guide.js

--- developer/guide.js
+++ developer/guide.js
@@ -1,5 +1,5 @@
 developerKingdomModule.
-	controller("guideCtrl", function guideCtrl($scope, $window){
+	controller("guideCtrl", function guideCtrl($scope, $window, $sce){
 		
 		$scope.sections = [{name: "Introduction", htmlText: "blah blah blah <h1>Lololol</h1>"},{name: "Adding plugins", html: "<h3>Na na na na na nana heeeeeey jude</h3><h3>Hey judey judey jude</h3>"}];
 		
@@ -13,5 +13,9 @@ developerKingdomModule.
 		
 		$scope.backToSections = function(){
 			$scope.currentSectionOn = false;
+		}
+		
+		$scope.to_trusted = function(html_code) {
+		    return $sce.trustAsHtml(html_code);
 		}
 	})
