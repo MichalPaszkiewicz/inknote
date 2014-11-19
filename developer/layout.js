7cc1b1eb0ed4705dbba054aebd94a23f4a283b8f
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-19 11:00:35 +0000

    Update layout.js

--- developer/layout.js
+++ developer/layout.js
@@ -2,14 +2,14 @@ developerKingdomModule.
 	controller("layoutCtrl", function layoutCtrl($scope, $window){
 
 		$scope.setSize = function(){
-			return ($window.innerWidth <= 1410);
+			$scope.fullWidth = ($window.innerWidth <= 1410);
 		}
 		
 		$scope.getSize = function(){
 			return ($window.innerWidth <= 1410)
 		}
 		
-		$scope.fullWidth = false || getSize();
+		$scope.fullWidth = false || $scope.getSize();
 
 		
 		var w = angular.element($window);
