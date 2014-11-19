ed5d5101e493a437631fcaf314d9b3d58095a31f
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-19 11:01:56 +0000

    Update layout.js

--- developer/layout.js
+++ developer/layout.js
@@ -5,7 +5,7 @@ developerKingdomModule.
 			$scope.fullWidth = ($window.innerWidth <= 1410);
 		}
 		
-		$scope.getSize = function($window){
+		$scope.getSize = function(){
 			return ($window.innerWidth <= 1410)
 		}
 		
