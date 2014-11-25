25757f222dffebfa5e3f257b6f7b23d30385566d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-25 14:11:45 +0000

    Update globalsettings.js

--- globalsettings.js
+++ globalsettings.js
@@ -1,3 +1,10 @@
 var staveColour = "black";
 var noteColour = "red";
 var textColour = "green";
+
+
+// get a new unique id
+function newID(){
+	return (new Date).getTime() + "" + Math.floor(100 * Math.random());
+}
+		
