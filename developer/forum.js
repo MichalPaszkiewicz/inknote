b68154ec566f817f364c876470a0a8ad41897b10
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-25 15:45:36 +0000

    Update forum.js

--- developer/forum.js
+++ developer/forum.js
@@ -18,33 +18,36 @@ var developerKingdomModule = angular.module('app', []).
 			}
 		}
 		
+		$scope.getPosts = function(){
+			var additionalPosts = [];
+			$http({method: "GET", url: "http://localhost:3000/posts"})
+				.success(function(data, status){
+					additionalPosts = data;
+					console.log("Additional posts: " + additionalPosts);
+				
+				for(var i = 0; i < additionalPosts.length; i++){
+					var relevantThreadIndex = getThreadIndexFromID(additionalPosts[i].threadID);
+					$scope.forum.threads[relevantThreadIndex].posts.push(additionalPosts[i]);
+				}
+			})
+			.error(function(data, status){
+				additionalPosts = data || "Request failed";
+				console.log(additionalPosts);
+			});
+		};
+		
 		$scope.fetch = function(){
 			var additionalThreads = [];
 			
-			$http({method: "GET", url: "http://localhost:3000/threads"}).success(function(data, status){
-				
+			$http({method: "GET", url: "http://localhost:3000/threads"})
+			.success(function(data, status){
 				additionalThreads = data;
 				console.log("Additional threads: " + additionalThreads)
 				for(var i = 0; i < additionalThreads; i++){
 					$scope.forum.threads.push(additionalThreads[i]);
 				}
-	
+				$scope.getPosts();
 				
-				var additionalPosts = [];
-				$http({method: "GET", url: "http://localhost:3000/posts"})
-					.success(function(data, status){
-						additionalPosts = data;
-						console.log("Additional posts: " + additionalPosts);
-					
-					for(var i = 0; i < additionalPosts.length; i++){
-						var relevantThreadIndex = getThreadIndexFromID(additionalPosts[i].threadID);
-						$scope.forum.threads[relevantThreadIndex].posts.push(additionalPosts[i]);
-					}
-				})
-				.error(function(data, status){
-					additionalPosts = data || "Request failed";
-					console.log(additionalPosts);
-				});
 			}).error(function(data, status){
 				console.log("Threads Fail");	
 			});
