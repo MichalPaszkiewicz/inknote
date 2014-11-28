592d01f76f44d12f5bca5733fc659269de237b50
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-28 09:29:20 +0000

    Update score.js

--- score.js
+++ score.js
@@ -525,6 +525,7 @@ var canvasModule = angular.module('app', ['monospaced.mousewheel', 'keypress']).
 		
 		$scope.drawBar = function(bar){
 			context.beginPath();
+			context.strokeStyle = staveColour;
 			context.moveTo(bar.x, bar.y);
 			context.lineTo(bar.x, bar.y + $scope.lineHeight);
 			context.stroke();
