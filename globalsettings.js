e7fe7fdfe9dfaa1b28054ba33fd205986bf944ac
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-11-28 14:04:42 +0000

    Update globalsettings.js

--- globalsettings.js
+++ globalsettings.js
@@ -6,6 +6,6 @@ var serverURL = "https://lit-basin-6551.herokuapp.com";
 
 // get a new unique id
 function newID(){
-	((new Date).getTime() + "").substr(3) + Math.floor(100000 * Math.random());
+	return ((new Date).getTime() + "").substr(3) + Math.floor(100000 * Math.random());
 }
 		
