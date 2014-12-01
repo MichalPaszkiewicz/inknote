1961c4d598745898e163e0a2f69b6aa6e4062e1e
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-01 09:46:09 +0000

    Update userfunctions.js

--- userfunctions.js
+++ userfunctions.js
@@ -5,6 +5,34 @@ var userFunctions = {};
 userFunctions.wakeUpCalls = [];
 userFunctions.afterSave = [];
 
+function getLocalUserFunctions(){
+	return tempUserFunctions = JSON.parse(localStorage.getItem("inknote-userfunctions"));
+}
+
+function addUserFunctionsFromJSON(JSONitem){
+	if(JSONitem && JSONitem.wakeUpCalls){
+		for(var i = 0; i < JSONitem.wakeUpCalls.length){
+			userFunctions.wakeUpCalls.push(JSONitem.wakeUpCalls[i]);
+		}
+		for(var i = 0; i < JSONitem.afterSave.length){
+			userFunctions.afterSave.push(JSONitem.afterSave[i]);
+		}	
+	}
+}
+
+function saveUserFunctions(newUserFunctions){
+	if(newUserFunctions){
+		localStorage.setItem("inknote-userfunctions", newUserFunctions);
+	}
+	else{
+		localStorage.setItem("inknote-userfunctions", userFunctions);
+	}
+}
+
+//on load add all user functions stored in local storage:
+addUserFunctionsFromJSON(getLocalUserFunctions());
+
+//go through all on load user functions
 for(var i = 0; i < userFunctions.wakeUpCalls.length; i++){
 	userFunctions.wakeUpCalls[i]();
 }
