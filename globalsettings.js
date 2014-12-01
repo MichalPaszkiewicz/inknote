81d7d526b9c88ae08835549f06a9b328efc442e5
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-01 16:55:17 +0000

    Update globalsettings.js

--- globalsettings.js
+++ globalsettings.js
@@ -2,6 +2,8 @@ var staveColour = "black";
 var noteColour = "red";
 var textColour = "green";
 
+var keypressFuncsOn = true;
+
 var serverURL = "https://lit-basin-6551.herokuapp.com";
 
 // get a new unique id
