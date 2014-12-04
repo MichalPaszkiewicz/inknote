0d116d6cbc8ef5cd1a05d94321618a0c946bb13a
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-04 14:43:39 +0000

    Create Gruntfile.js

---
+++ tests/Gruntfile.js
@@ -0,0 +1,21 @@
+// Gruntfile.js
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Mocha
+    mocha: {
+      all: {
+        src: ['tests/testrunner.html'],
+      },
+      options: {
+        run: true
+      }
+    }
+  });
+
+  // Load grunt mocha task
+  grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.registerTask('default', ['mocha']);
+};
