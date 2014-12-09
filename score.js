49972fb58a5ae2dc0ebbb4e198b5a9dc3b238504
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-09 17:52:42 +0000

    Update score.js

--- score.js
+++ score.js
@@ -548,6 +548,11 @@ var canvasModule = angular.module('app', ['monospaced.mousewheel', 'keypress']).
 				var relevantBar = relevantInstrument.bars[relevantBarIndex];
 				if(relevantBar){
 					var relevantItemIndex = relevantBar.items.getIndexFromID($scope.selectedItemID);
+					var currentSelection = {barIndex: relevantBarIndex, itemIndex: relevantItemIndex};
+					var itemFound = $scope.moveRight(relevantInstrument, currentSelection);
+					if(!itemFound){
+						$scope.moveLeft(relevantInstrument, currentSelection);	
+					}
 					relevantBar.items.splice(relevantItemIndex, 1);
 					$scope.draw();
 				}
