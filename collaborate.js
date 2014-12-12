b7e371b698f97faa8e1d91f2b452462b0fc4d0b8
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-12 14:40:11 +0000

    Clearing out jquery from collaborator.

--- collaborate.js
+++ collaborate.js
@@ -3,40 +3,67 @@ angular.module('peerJS', []).
 		// Connect to PeerJS, have server assign an ID instead of providing one
 		// Showing off some of the configs available with PeerJS :).
 		var peer = new Peer({
-		    // Set API key for cloud server (you don't need this if you're running your
-		    // own.
-		    key: '9t3v4nsdk6ueg66r',
+			// Set API key for cloud server 
+		    	key: '9t3v4nsdk6ueg66r',
 		
-		    // Set highest debug level (log everything!).
-		    debug: 3,
+			// Set highest debug level (log everything!).
+			debug: 3,
 		
-		    // Set a logging function:
-		    logFunction: function() {
-		        var copy = Array.prototype.slice.call(arguments).join(' ');
-		        $('.log').append(copy + '<br>');
-		    },
-		
-		    // Use a TURN server for more network support
-		    config: {'iceServers': [
-		      { url: 'stun:stun.l.google.com:19302' }
-		    ]} /* Sample servers, please use appropriate ones */
+			// Set a logging function:
+			logFunction: function() {
+				var copy = Array.prototype.slice.call(arguments).join(' ');
+				console.log(copy);
+			}
 		});
 		
+		//current client connection id.
+		$scope.myID = null;
+		
+		//object holding all client ids.
 		var connectedPeers = {};
+		
+		$scope.collaboratorMessages = [];
 	
 		// Show this peer's ID.
 		peer.on('open', function(id){
-		    $(".me").val(id);
-		    
-		    	$(".friend-link").val(location.href + "?id=" + id);
-			
-			$(".friend-link").click(function(){ $(this).select() });
-			
+			$scope.myID = id;
 		});
 	
 		// Await connections from others
 		peer.on('connection', connect);
 	
+		// Handle a connection object.
+		function connect(c) {
+			
+		    // Handle a chat connection.
+			if (c.label === 'chat') {
+				$scope.collaboratorMessages.push({peer: c.peer, message: c.peer + " has connected."});
+
+				c.on('data', function(data) {
+					$scope.collaboratorMessages.push({peer: c.peer, message: data});
+			        });
+				
+				c.on('close', function() {
+					alert(c.peer + ' has left the chat.');
+					delete connectedPeers[c.peer];
+			        });
+			} else if (c.label === 'file') {
+				c.on('data', function(data) {
+					// If we're getting a file, create a URL for it.
+					if (data.constructor === ArrayBuffer) {
+						var dataView = new Uint8Array(data);
+						var dataBlob = new Blob([dataView]);
+						var url = window.URL.createObjectURL(dataBlob);
+						$scope.collaboratorMessages.push({peer: c.peer, message: c.peer + ' has sent you a <a target="_blank" href="' + url + '">file</a>.'});
+					}
+				});
+			}
+		    
+			if(location.search.length == 0)
+			{
+				setTimeout(function(){sendSettings()},500);
+			}
+		}
 	
 	
 	});
