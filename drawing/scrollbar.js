ab51b9285d648eaf05d5e74cdb28e09c6637704d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-16 13:30:57 +0000

    Update scrollbar.js

--- drawing/scrollbar.js
+++ drawing/scrollbar.js
@@ -1,12 +1,13 @@
 var drawScrollBar = function(canvas, context, scrollPosition){
 	var height = 400;
 	var width = 20;
+	var distanceFromTop = 100;
 	scrollPosition = 50;
 	
-	context.rect(20,20,150,100);
+	context.rect(canvas.width - width, distanceFromTop, canvas.width, distanceFromTop + height);
 	context.stroke();
 	context.beginPath();
-	context.moveTo(canvas.width - width, scrollPosition);
-	context.lineTo(canvas.width, scrollPosition); 
+	context.moveTo(canvas.width - width, scrollPosition + distanceFromTop);
+	context.lineTo(canvas.width, scrollPosition + distanceFromTop); 
 	context.stroke();
 }
