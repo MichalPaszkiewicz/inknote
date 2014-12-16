93823d02918a2fc72ac467ba91d92118762d7cba
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-16 13:15:46 +0000

    Update globalsettings.js

--- globalsettings.js
+++ globalsettings.js
@@ -1,9 +1,17 @@
+//set up canvas
+var canvas = document.getElementById('canvas');
+canvas.width = $window.innerWidth;
+canvas.height = $window.innerHeight;
+var context = canvas.getContext('2d');
+
+//set up drawing colours
 var staveColour = "black";
 var noteColour = "red";
 var textColour = "green";
 
 var keypressFuncsOn = true;
 
+//url to server (for bug reports)
 var serverURL = "https://lit-basin-6551.herokuapp.com";
 
 // get a new unique id
