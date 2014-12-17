41f5e47768242f6827f221e7df26df5ee88b4360
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-17 18:51:43 +0000

    Update bar.js

--- drawing/bar.js
+++ drawing/bar.js
@@ -15,4 +15,10 @@ var drawSingleBar = function(canvas, context, x, y, bar, width, height){
 		context.stroke();
 		tempY += height / 4;
 	}
+	
+	context.fillStyle = noteColour;
+	log("Time signature font: " + "bold " + (2 * $scope.lineHeight/3) + "px Arial");
+	context.font = "bold " + (2 * height/3) + "px Arial";
+	context.fillText(bar.timeSignature.top, x, y + height/2);
+	context.fillText(bar.timeSignature.bottom, x, y + height);
 }
