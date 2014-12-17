098f926be4aacbb33a7cef29d1a921d842303885
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-12-17 18:36:07 +0000

    Update bar.js

--- drawing/bar.js
+++ drawing/bar.js
@@ -1,5 +1,18 @@
 //dependencies: item.js
-var drawSingleBar = function(canvas, context, x, y, bar){
+var drawSingleBar = function(canvas, context, x, y, bar, width, height){
+	context.beginPath();
+	var startX = x;
+	var finalX = x + width;
+	var startY = y;
 	
+	var tempY = startY;
 	
+	for(var j = 0; j < 5; j++){
+		context.beginPath();
+		context.moveTo(x, tempY);
+		context.lineTo(finalX, tempY);
+		context.strokeStyle = staveColour;
+		context.stroke();
+		tempY += height / 4;
+	}
 }
