b17721fa937acefa7bcec455567cf99d3bef063b
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-01-19 17:24:00 +0000

    Update userfunctions.js

--- userfunctions.js
+++ userfunctions.js
@@ -1,5 +1,3 @@
-/*todo: in future, this file will hold many arrays of user defined functions. The user will be able to add functions to the arrays to, for example, save data to their own database on save, etc. */
-
 var userFunctions = {};
 
 userFunctions.wakeUpCalls = []; //before load
