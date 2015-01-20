d24714419e407216d71cf9db8c7601a1cb169dd6
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-01-20 13:21:01 +0000

    Create rests.js

---
+++ drawing/rests.js
@@ -0,0 +1,27 @@
+function drawSemiBreveRest(cvs, ctx){
+
+}
+
+function drawCrotchetRest(cvs, ctx){
+
+}
+
+function drawQuaverRest(cvs, ctx){
+
+}
+
+function drawSemiQuaverRest(cvs, ctx){
+
+}
+
+function drawDemiSemiQuaverRest(cvs, ctx){
+
+}
+
+function drawHemiDemiSemiQuaverRest(cvs, ctx){
+
+}
+
+function drawRest(cvs, ctx, duration){
+
+}
