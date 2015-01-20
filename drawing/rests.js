1f01d031172d2175b8312382db316f951417c6d9
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-01-20 13:24:47 +0000

    Update rests.js

--- drawing/rests.js
+++ drawing/rests.js
@@ -1,27 +1,39 @@
-function drawSemiBreveRest(cvs, ctx, x, y){
+function drawSemiBreveRest(cvs, ctx, x, y, height){
 
 }
 
-function drawCrotchetRest(cvs, ctx, x, y){
+function drawCrotchetRest(cvs, ctx, x, y, height){
 
 }
 
-function drawQuaverRest(cvs, ctx, x, y){
+function drawQuaverRest(cvs, ctx, x, y, height){
+	ctx.beginPath();
+	ctx.arc(x, y, height / 4, 0, 2 * Math.PI);
+	ctx.fill();
+	
+	ctx.moveTo(x, y); 
+	ctx.bezierCurveTo( x + height / 2, y + height , x + 3 * height / 2, y + height / 2, x + 2 * height, y); 
+	ctx.bezierCurveTo( x + 3 * height / 2, y + height / 2 , x + height / 2, y + height, x - height / 5, y + height * 0.75 / 4); 
+	ctx.stroke();
+	ctx.fill();
+	
+	ctx.moveTo(x + 2 * height, y);
+	ctx.lineTo(x, y + height * 4);
+	ctx.stroke();
+}
+
+function drawSemiQuaverRest(cvs, ctx, x, y, height){
 
 }
 
-function drawSemiQuaverRest(cvs, ctx, x, y){
+function drawDemiSemiQuaverRest(cvs, ctx, x, y, height){
 
 }
 
-function drawDemiSemiQuaverRest(cvs, ctx, x, y){
+function drawHemiDemiSemiQuaverRest(cvs, ctx, x, y, height){
 
 }
 
-function drawHemiDemiSemiQuaverRest(cvs, ctx, x, y){
-
-}
-
-function drawRest(cvs, ctx, x, y, duration){
+function drawRest(cvs, ctx, x, y, duration, height){
 
 }
