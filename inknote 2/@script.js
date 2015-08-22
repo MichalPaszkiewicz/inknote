a30cc9f930a8f3a1c13e5e74942e1b6c20be137d
unknown <michalpaszkiewicz@hotmail.com>
2015-08-22 18:22:47 +0100

    drawing treble clef

--- inknote 2/@script.js
+++ inknote 2/@script.js
@@ -1251,11 +1251,38 @@ var Inknote;
             }
             GClef.prototype.draw = function (ctx) {
                 var hlh = this.lineHeight / 2;
+                var diag = hlh * Math.sin(Math.PI / 4);
                 ctx.beginPath();
-                ctx.moveTo(this.x - hlh, this.y + hlh);
-                ctx.bezierCurveTo(this.x - hlh * 2, this.y - hlh, this.x - hlh, this.y - hlh * 2, this.x + hlh, this.y - hlh);
-                ctx.strokeStyle = "green";
+                ctx.fillStyle = Drawing.Colours.black;
+                ctx.strokeStyle = Drawing.Colours.black;
+                if (this.hover || this.select) {
+                    ctx.fillStyle = Drawing.Colours.orange;
+                    ctx.strokeStyle = Drawing.Colours.orange;
+                }
+                //outer
+                ctx.moveTo(this.x + diag / 2, this.y + 3 * hlh);
+                ctx.lineTo(this.x - diag, this.y - 4 * hlh);
+                ctx.bezierCurveTo(this.x - 3 * hlh, this.y - 11 * hlh, this.x + 5 * hlh, this.y - 9 * hlh, this.x, this.y - 4 * hlh);
+                ctx.bezierCurveTo(this.x - 2 * hlh, this.y - 3 * hlh, this.x - 4 * hlh, this.y - 2 * hlh, this.x - 2 * hlh, this.y + diag);
+                ctx.bezierCurveTo(this.x - 2 * diag, this.y + 3 * diag, this.x, this.y + 2 * diag, this.x + diag, this.y + diag);
+                ctx.bezierCurveTo(this.x + 4 * diag, this.y - 2 * hlh, this.x - 4 * diag, this.y - 2 * hlh, this.x - diag, this.y + diag);
+                // inner
+                ctx.bezierCurveTo(this.x - 3 * diag, this.y - hlh, this.x + 3 * diag, this.y - hlh, this.x + diag, this.y + diag);
+                ctx.bezierCurveTo(this.x, this.y + 2 * diag, this.x - 2 * diag, this.y + 3 * diag, this.x - 2 * hlh, this.y + diag);
+                ctx.bezierCurveTo(this.x - 3 * hlh, this.y - 2 * hlh, this.x - hlh, this.y - 3 * hlh, this.x, this.y - 4 * hlh);
+                ctx.bezierCurveTo(this.x + 4 * hlh, this.y - 8 * hlh, this.x - 2 * hlh, this.y - 10 * hlh, this.x - diag, this.y - 4 * hlh);
+                ctx.lineTo(this.x + diag / 2, this.y + 3 * hlh);
+                ctx.fill();
                 ctx.stroke();
+                // connecting line
+                ctx.beginPath();
+                ctx.moveTo(this.x + diag / 2, this.y + 3 * hlh);
+                ctx.bezierCurveTo(this.x + diag, this.y + 5 * hlh, this.x - 2 * diag, this.y + 5 * hlh, this.x - hlh, this.y + 4 * hlh);
+                ctx.stroke();
+                // ball
+                ctx.beginPath();
+                ctx.arc(this.x - hlh, this.y + 4 * hlh, diag, 0, 2 * Math.PI);
+                ctx.fill();
                 return true;
             };
             return GClef;
